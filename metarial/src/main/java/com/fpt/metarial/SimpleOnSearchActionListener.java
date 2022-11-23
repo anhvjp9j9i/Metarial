bf85640bd3b9227876b4a48e5c7ab67c664f846b
anhvjp9j9i <sangff2003@gmail.com>
2022-11-23 08:58:01 +0700

    first commit

---
+++ metarial/src/main/java/com/fpt/metarial/SimpleOnSearchActionListener.java
@@ -0,0 +1,19 @@
+package com.fpt.metarial;
+
+public abstract class SimpleOnSearchActionListener implements MaterialSearchBar.OnSearchActionListener {
+
+    @Override
+    public void onSearchStateChanged(boolean enabled) {
+
+    }
+
+    @Override
+    public void onSearchConfirmed(CharSequence text) {
+
+    }
+
+    @Override
+    public void onButtonClicked(int buttonCode) {
+
+    }
+}
